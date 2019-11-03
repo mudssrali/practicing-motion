7de3a2100c8a943501b268b3694a09ffd6ebfaeb
Mudassar045 <mudassaralics007@gmail.com>
2019-11-04 00:47:43 +0500

    add package react-motion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Motion, spring, StaggeredMotion} from 'react-motion';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <StaggeredMotion
+    defaultStyles={[{h: 0}, {h: 0}, {h: 0}]}
+    styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+      return i === 0
+        ? {h: spring(100)}
+        : {h: spring(prevInterpolatedStyles[i - 1].h)}
+    })}>
+    {interpolatingStyles =>
+      <div>
+        {interpolatingStyles.map((style, i) =>
+          <div key={i} style={{border: '1px solid', height: style.h}}>
+            <Motion defaultStyle={{x: 0}} style={{x: spring(10000000)}}>
+              {value => <div>{(value.x * value.x)}</div>}
+            </Motion>
+          </div>)
+        }
+      </div>
+    }
+  </StaggeredMotion>
     </div>
+    </>
   );
 }
 
